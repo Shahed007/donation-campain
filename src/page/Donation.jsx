8799e426e23734cebbfadba9ee5302f337018a26
Shahed <sk0178397@gmial.com>
2023-09-25 23:33:08 +0600

    Added show all button on the doantion page. And added functionality show all button if donationData grater than 4 then showing show all button. if user click the button will be hide.

--- src/page/Donation.jsx
+++ src/page/Donation.jsx
@@ -4,6 +4,7 @@ import DonationCard from "../components/Card/DonationCard/DonationCard";
 import { useOutletContext } from "react-router-dom";
 
 const Donation = () => {
+  const [showAll, setShowAll] = useState(false);
   const [donateData, setDonateData] = useState([]);
   const data = useOutletContext();
 
@@ -18,10 +19,18 @@ const Donation = () => {
     },[data])
  
   return (
-    <section className="container mx-auto px-3 mt-20 grid grid-cols-1 md:grid-cols-2 gap-6">
+    <section className="container mx-auto px-3 mt-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
       {
+        showAll ? 
         donateData?.map(donate => <DonationCard key={donate.id} donate={donate}></DonationCard>)
+        :
+        donateData?.slice(0,4)?.map(donate => <DonationCard key={donate.id} donate={donate}></DonationCard>)
       }
+      </div>
+      <div className={` justify-center my-10 ${donateData?.length > 4 ? 'flex':'hidden'} ${showAll ? 'hidden' : ''}`}>
+        <button onClick={()=> setShowAll(!showAll)} className=" bg-[#009444] active:scale-95 px-9 py-3 text-base rounded-lg text-white font-semibold">See All</button>
+      </div>
     </section>
   )
 }
