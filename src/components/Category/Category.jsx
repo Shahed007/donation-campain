9aa8c842887fbe65640bfbb574651324b75990a0
Shahed <sk0178397@gmial.com>
2023-09-25 16:03:53 +0600

    Added not found page if card data not found.

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -7,6 +7,7 @@ const Category = () => {
   const [searchText, setSearchText] = useState('');
   const [searchInput, setSearchInput] = useState('');
   const [getData, setData] = useState([]);
+  const [notFound, setNotFound] = useState(false);
   const form_data = document.getElementById('form-data');
   const search = document.getElementById('search');
   const data = useOutletContext();
@@ -23,7 +24,9 @@ const Category = () => {
       });
 
       if(!searchInput){
-          setData(data)
+          setData(data);
+          setNotFound(false);
+
       }else{
         const filterData = data.filter(item => {
           if(item.category.toLowerCase().includes(searchText.toLowerCase())){
@@ -33,16 +36,30 @@ const Category = () => {
           }
         });
 
-        setData(filterData);
+         if(!filterData.length){
+          setNotFound(true)
+         }else{
+          setData(filterData);
+         }
+          
+        
+        
       }
     }
   },[data.length, data, form_data, search, searchInput, searchText]);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-    {
-      getData?.map(item => <Card key={item.id} item={item}></Card>)
-    }
-    </div>
+    <>
+      {
+        notFound ? 
+        <h2 className="text-center h-screen flex justify-center items-center">Search data not found</h2>
+        :
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {
+           getData?.map(item => <Card key={item.id} item={item}></Card>)
+        }
+       </div>
+      }
+    </>
   )
 }
 
