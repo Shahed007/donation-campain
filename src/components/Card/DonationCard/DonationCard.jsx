2cd079d16b2dc0f84c92f365f47af8850de0bd2a
Shahed <sk0178397@gmial.com>
2023-09-26 20:53:06 +0600

    Solved reponsive problem on the donation page.

--- src/components/Card/DonationCard/DonationCard.jsx
+++ src/components/Card/DonationCard/DonationCard.jsx
@@ -6,14 +6,14 @@ import { Link } from 'react-router-dom';
 const DonationCard = ({donate}) => {
   const {id,image,title,card_bg,text_color,button_bg,category_bg, category, price} = donate || {};
   return (
-    <div className='rounded-lg overflow-hidden flex gap-6' style={{backgroundColor: `${card_bg}`}}>
+    <div className='h-52 sm:h-full rounded-lg overflow-hidden flex gap-6' style={{backgroundColor: `${card_bg}`}}>
       <div className='w-2/5'><img className='h-full w-full rounded-l-lg scale-105' src={image} alt={`image of ${title}`} /></div>
-      <div className='py-6 flex-1'>
+      <div className='py-3 sm:py-6 flex-1 flex flex-col'>
         <h4><span className='text-sm py-1 px-[10px] rounded' style={{backgroundColor: `${category_bg}`, color: `${text_color}`}}>{category}</span></h4>
-        <h2 className='text-2xl font-semibold text-[#0B0B0B] mt-3'>{title}</h2>
-        <h3 className='mt-2 text-base font-semibold mb-7' style={{color: `${text_color}`}}>${price}</h3>
+        <h2 className='text-xl sm:text-2xl font-semibold grow text-[#0B0B0B] mt-3'>{title}</h2>
+        <h3 className='mt-2 text-base font-semibold mb-4 md:mb-7' style={{color: `${text_color}`}}>${price}</h3>
         <Link to={`/donationDetails/${id}`} state={title}>
-        <button className='px-6 py-3 text-lg font-semibold text-white rounded active:scale-95' style={{backgroundColor: `${button_bg}`}}>View Details</button>
+        <button className='px-6 py-3 text-base sm:text-xl font-semibold text-white rounded active:scale-95' style={{backgroundColor: `${button_bg}`}}>View Details</button>
         </Link>
       </div>
     </div>
