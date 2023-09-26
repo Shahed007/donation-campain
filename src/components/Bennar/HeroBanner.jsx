79ae03e2dbe62f090a270db89c65d1df656ee7bb
Shahed <sk0178397@gmial.com>
2023-09-26 16:14:28 +0600

    added active calss on search button

--- src/components/Bennar/HeroBanner.jsx
+++ src/components/Bennar/HeroBanner.jsx
@@ -8,7 +8,7 @@ const HeroBanner = ({handleSubmit, handleOnChange}) => {
   
       <form autoCapitalize="off" onSubmit={handleSubmit}  className="flex w-full justify-center">
       <input onChange={handleOnChange}  id='searchBox'  name="search" className="w-1/2  px-4 py-2 border-2 shadow-md shadow-[#FF444A]/30 border-[#FF444A] rounded-l-lg" placeholder="Search hear" type="text" />
-      <input className="px-6 py-3 bg-[#FF444A] rounded-r-lg text-white text-base font-semibold"  type="submit" value="Search" />
+      <input className="px-6 py-3 bg-[#FF444A] active:scale-95 rounded-r-lg text-white text-base font-semibold"  type="submit" value="Search" />
       </form>
  
     </div>
