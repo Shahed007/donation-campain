69c5f17d894571db2778857a4ea2262b448ae700
Shahed <sk0178397@gmial.com>
2023-09-27 23:10:31 +0600

    Changed font size hero section title for the mobile device.

--- src/components/Bennar/HeroBanner.jsx
+++ src/components/Bennar/HeroBanner.jsx
@@ -4,7 +4,7 @@ const HeroBanner = ({handleSubmit, handleOnChange}) => {
  
   return (
     <div className="max-w-3xl mx-auto px-3 flex flex-col justify-center items-center h-full">
-    <h1 className="font-bold text-4xl  text-[#0B0B0B] text-center mb-10">I Grow By Helping People In Need</h1>
+    <h1 className="font-bold text-3xl md:text-4xl  text-[#0B0B0B] text-center mb-10">I Grow By Helping People In Need</h1>
   
       <form autoCapitalize="off" onSubmit={handleSubmit}  className="flex w-full justify-center">
       <input onChange={handleOnChange}  id='searchBox'  name="search" className="w-1/2  px-4 py-2 border-2 shadow-md shadow-[#FF444A]/30 border-[#FF444A] rounded-l-lg" placeholder="Search hear" type="text" />
